4390b309cf51f640961e8b5dce9f57e3b3014bbb
MiladTootoonchi <milad.tootoonchi04@gmail.com>
2025-09-29 21:32:26 +0200

    changed the main so it will predict for the updated data

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ from src.model import create_XGBoost
 def main():
     data_path = "data/historical_flights.csv"
 
-
     config = TrainConfig(flight_csv = data_path, 
                          model_name = "xg", 
                          model_function = create_XGBoost)
     model = train(config)
 
-    model_predict("data/schedule_oct2025.csv", model)
+    model_predict("data/schedule_oct2025_updated.csv", model)
     
 
 if __name__ == "__main__":
